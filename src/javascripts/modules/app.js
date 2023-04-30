ccc29df78e99a8f42a912aac9f372801052152f8
iridon0920 <iridon0920@gmail.com>
2023-04-30 09:32:21 +0900

    change: SendGrid API関係の処理をヘルパー関数として抽出

--- src/javascripts/modules/app.js
+++ src/javascripts/modules/app.js
@@ -11,40 +11,6 @@ class App {
 
   async init () {
 
-    // sendgridでメールを送信する
-    const options = {
-      url: 'https://api.sendgrid.com/v3/mail/send',
-      type: 'POST',
-      headers: {
-        Authorization: 'Bearer {{setting.sendgridApiKey}}'
-      },
-      contentType: 'application/json',
-      data: JSON.stringify({
-        personalizations: [
-          {
-            to: [
-              {
-                email: 'irii.keita@classmethod.jp'
-              }
-            ],
-            subject: 'test'
-          }
-        ],
-        from: {
-          email: 'irii.keita@classmethod.jp'
-        },
-        content: [
-          {
-            type: 'text/plain',
-            value: 'test'
-          }
-        ]
-      }),
-      secure: true
-    }
-    this._client.request(options).then((res) => {
-      console.log(res)
-    })
   }
 }
 
