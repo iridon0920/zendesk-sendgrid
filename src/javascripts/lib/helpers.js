ff8a1019666a77b39c42dbeb5d7b24394a9db446
iridon0920 <iridon0920@gmail.com>
2023-04-30 09:14:49 +0900

    add: SendGrid API連携テスト

--- src/javascripts/lib/helpers.js
+++ src/javascripts/lib/helpers.js
@@ -54,3 +54,30 @@ export function escapeSpecialChars (str) {
 
   return str.replace(/[&<>"'`=]/g, function (m) { return escape[m] })
 }
+
+/**
+ * Helper to convert snake case strings to sentence case
+ * @param {String} string Snake case string
+ * @return {String} Sentence case string
+ */
+export function toSentenceCase (string) {
+  const finalString = string.replace(/_/g, ' ')
+  return finalString.charAt(0).toUpperCase() + finalString.slice(1)
+}
+
+/**
+ * Helper to format ISO 8601 timestamps as human-readable datetime
+ * @param {String} date ISO 8601 timestamp
+ * @return {String} Formatted date. Example: Sep 1, 2099 2:55 PM
+ */
+export function formatDate (date) {
+  const formattedDate = new Date(date)
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  }
+  return formattedDate.toLocaleDateString('en-us', options)
+}
