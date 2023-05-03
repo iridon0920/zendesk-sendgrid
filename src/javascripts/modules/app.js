b4c691945da0b1bb43df01176fd8936e20c01672
iridon0920 <iridon0920@gmail.com>
2023-05-03 15:16:35 +0900

    add: 選択式ユーザーテーブル作成

--- src/javascripts/modules/app.js
+++ src/javascripts/modules/app.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Hello } from "./hello";
 import { render } from "react-dom";
 import { resizeContainer } from "../lib/helpers";
+import { UsersTable } from "./users_table";
 
 const MAX_HEIGHT = 1000;
 
@@ -12,10 +12,26 @@ class App {
     this.initializePromise = this.init();
   }
 
+  rowData = Array.from(Array(10)).map((row, index) => ({
+    id: `row-${index}`,
+    name: `User #${index}`,
+    email: "test@example.com",
+    selected: false,
+  }));
+
+  handleSelectedUsersChange(users) {
+    console.log(users);
+  }
+
   async init() {
     const container = document.querySelector(".main");
-
-    render(<Hello name="World" />, container);
+    render(
+      <UsersTable
+        users={this.rowData}
+        onSelectedUsersChange={this.handleSelectedUsersChange}
+      />,
+      container
+    );
     return resizeContainer(this._client, MAX_HEIGHT);
   }
 }
---
+++ src/javascripts/modules/users_table.tsx
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import {
+  Body,
+  Cell,
+  Head,
+  HeaderCell,
+  HeaderRow,
+  Row,
+  Table,
+} from "@zendeskgarden/react-tables";
+import { KEY_CODES } from "@zendeskgarden/container-utilities";
+import { Field, Checkbox, Label } from "@zendeskgarden/react-forms";
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+interface IUserRow extends IUser {
+  selected: boolean;
+}
+
+interface UsersTableProps {
+  users: IUser[];
+  onSelectedUsersChange: (selectedUsers: IUser[]) => void;
+}
+
+const isSelectAllIndeterminate = (rows: IUserRow[]) => {
+  const numSelectedRows = rows.reduce((accumulator, row) => {
+    if (row.selected) {
+      return accumulator + 1;
+    }
+
+    return accumulator;
+  }, 0);
+
+  return numSelectedRows > 0 && numSelectedRows < rows.length;
+};
+
+const isSelectAllChecked = (rows: IUserRow[]) =>
+  rows.every((row) => row.selected);
+
+const getSelectedUsers = (rows: IUserRow[]): IUser[] => {
+  return rows
+    .filter((row) => row.selected)
+    .map((row) => ({
+      id: row.id,
+      name: row.name,
+      email: row.email,
+    }));
+};
+
+export const UsersTable: React.FC<UsersTableProps> = ({
+  users,
+  onSelectedUsersChange,
+}) => {
+  const [data, setData] = useState(
+    users.map((user) => ({ ...user, selected: false }))
+  );
+  const [shiftEnabled, setShiftEnabled] = useState(false);
+  const [focusedRowIndex, setFocusedRowIndex] = useState<number | undefined>(
+    undefined
+  );
+
+  return (
+    <div style={{ overflowX: "auto" }}>
+      <Table style={{ minWidth: 500 }}>
+        <Head>
+          <HeaderRow>
+            <HeaderCell isMinimum>
+              <Field>
+                <Checkbox
+                  indeterminate={isSelectAllIndeterminate(data)}
+                  checked={isSelectAllChecked(data)}
+                  onChange={(e) => {
+                    const updatedRows = data.map((row) => ({
+                      ...row,
+                      selected: e.target.checked,
+                    }));
+
+                    setData(updatedRows);
+                    const selectedUsers = getSelectedUsers(updatedRows);
+                    onSelectedUsersChange(selectedUsers);
+                  }}
+                >
+                  <Label hidden>Select all tickets</Label>
+                </Checkbox>
+              </Field>
+            </HeaderCell>
+            <HeaderCell>Name</HeaderCell>
+            <HeaderCell>Email</HeaderCell>
+          </HeaderRow>
+        </Head>
+        <Body>
+          {data.map((row, index) => (
+            <Row key={row.id} isSelected={row.selected}>
+              <Cell isMinimum>
+                <Field>
+                  <Checkbox
+                    checked={row.selected}
+                    onKeyDown={(e) => {
+                      if (e.keyCode === KEY_CODES.SHIFT) {
+                        setShiftEnabled(true);
+                      }
+                    }}
+                    onKeyUp={() => {
+                      setShiftEnabled(false);
+                    }}
+                    onChange={(e) => {
+                      const updatedRows = [...data];
+
+                      if (shiftEnabled && focusedRowIndex !== undefined) {
+                        const startIndex = Math.min(focusedRowIndex, index);
+                        const endIndex = Math.max(focusedRowIndex, index);
+
+                        const isAllChecked = updatedRows
+                          .slice(startIndex, endIndex + 1)
+                          .every((slicedRow) => slicedRow.selected);
+
+                        for (let x = startIndex; x <= endIndex; x++) {
+                          if (x === index && isAllChecked) {
+                            updatedRows[x].selected = true;
+                            continue;
+                          }
+
+                          updatedRows[x].selected = !isAllChecked;
+                        }
+                      } else if (e.target.checked === true) {
+                        updatedRows[index].selected = true;
+                      } else {
+                        updatedRows[index].selected = false;
+                      }
+
+                      setData(updatedRows);
+                      setFocusedRowIndex(index);
+
+                      const selectedUsers = getSelectedUsers(updatedRows);
+                      onSelectedUsersChange(selectedUsers);
+                    }}
+                  >
+                    <Label hidden>Select ticket for {row.name}</Label>
+                  </Checkbox>
+                </Field>
+              </Cell>
+              <Cell>{row.name}</Cell>
+              <Cell>{row.email}</Cell>
+            </Row>
+          ))}
+        </Body>
+      </Table>
+    </div>
+  );
+};
