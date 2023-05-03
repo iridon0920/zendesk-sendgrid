84f72ccb71ba103c905634d4aa8b7bde846d50c8
iridon0920 <iridon0920@gmail.com>
2023-05-03 15:08:10 +0900

    change: ESLint 戻り値の型表記強制ルールを無効化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'prettier'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-  }
-}
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
